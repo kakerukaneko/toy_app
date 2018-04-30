97a0400a8c09dfccf6de6148ed9add48ed2a2fbe
kakerukaneko <csositai@gmail.com>
2018-04-30 14:29:27 +0900

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-<<<<<<< HEAD
-=======
+  
   def hello
     render html: "hello, world!"
-  end
->>>>>>> origin/master
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-  resources :microposts
-  resources :users
-  root 'users#index'
->>>>>>> origin/master
+  root 'application#hello'
 end
