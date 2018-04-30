6b6154086ffeceac214b68b3b682beebe381c195
kakeru kaneko <csositai@gmail.com>
2018-04-30 07:36:26 +0000

    6 cloud0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "こんにちは5 cloud9"
+    render html: "こんにちは6 cloud9"
   end
 end
