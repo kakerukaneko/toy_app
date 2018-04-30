f8621ed08549ee4a6425bde6b75ed431250e61d2
kakerukaneko <csositai@gmail.com>
2018-04-30 16:39:51 +0900

    6 local

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "こんにちは5 local"
+    render html: "こんにちは6 local"
   end
 end
