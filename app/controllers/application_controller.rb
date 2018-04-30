9751a8ffa6b42c0e2760507df21cb2b8af24bb8a
kakeru kaneko <csositai@gmail.com>
2018-04-30 07:42:35 +0000

    7 cloud9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "こんにちは6 local"
+    render html: "こんにちは7 cloud9"
   end
 end
